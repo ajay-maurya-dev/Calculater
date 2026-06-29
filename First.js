7c37ec61eb2a1ca614b2ee7a569cddb96d4baa88
Ajay Maurya <ajaymaurya23125@gmail.com>
2026-06-29 09:02:41 +0530

    Removed .vscode folder

--- First.js
+++ First.js
@@ -1,5 +1,16 @@
 let input = document.getElementById("input")
 let button = document.querySelectorAll(".btns")
+let mode = document.getElementById("mode")
+
+mode.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        mode.innerText = "Light";
+    } else {
+        mode.innerText = "Dark";
+    }
+});
 
 button.forEach((btn) => {
     btn.addEventListener("click", () => {
